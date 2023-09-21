97655c7141d7c59066269dfeb716b4cc3200217b
Riyad Ahmed <rbriyad2@gmail.com>
2023-09-21 23:37:46 +0600

    password problem solve

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,22 +1,28 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../providers/AuthProvider';
 import googleIcon from '../../images/google_300221.png';
 import githubIcon from '../../images/github_733609.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import './Login.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
+
   const {signIn, googleSignIn, githubSignIn}= useContext(AuthContext)
 const navigate = useNavigate()
 const location = useLocation()
-console.log(location)
+const [error, setError]= useState('')
+
 
 const from = location.state?.from?.pathname || '/';
     const handleLogin = event =>{
       event.preventDefault()
         const email = event.target.email.value;
         const password =event.target.password.value;
-        console.log(email , password)
+       
 
         signIn(email , password)
         .then( result=>{
@@ -26,8 +32,18 @@ const from = location.state?.from?.pathname || '/';
           navigate(from, { replace: true })
         })
         .catch(error=>{
-          console.log(error)
+          setError(error.message)
         })
+        toast.success('Login Successfull', {
+          position: "top-center",
+          autoClose: 1000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+          });
     }
 
     const handleGoogleLogin =()=>{
@@ -37,7 +53,7 @@ const from = location.state?.from?.pathname || '/';
         console.log(loguser)
       })
       .catch(error=>{
-        console.log(error.message)
+        setError(error.message)
       })
       navigate(from, { replace: true })
     }
@@ -48,11 +64,15 @@ const from = location.state?.from?.pathname || '/';
         console.log(loguser)
       })
       .catch(error=>{
-        console.log(error.message)
+        setError(error.message)
       })
       navigate(from, { replace: true })
     }
 
+    const [show, setShow]= useState(false)
+    const handleshowPassword=()=>{
+      setShow(!show)
+    }
 
     return (
         <div className="signuppage">
@@ -66,10 +86,13 @@ const from = location.state?.from?.pathname || '/';
             </div>
             <div className="form-control">
               <label htmlFor="password">Password</label>
-              <input type="password" name="password" required/>
+              <input id='passwordinput' type={show ? 'password' : 'text'} name="password" required/>
+              {show? <FontAwesomeIcon onClick={handleshowPassword} icon={faEyeSlash} id='eyeoff'/> : <FontAwesomeIcon onClick={handleshowPassword} icon={faEye} id='eyeoff'/> }
             </div>
+            <p id="errortext">{error}</p>
             <button type='submit' className="submitbtn">Login</button>
           </form>
+          <ToastContainer />
           <p>New t Ema-Jhon ? <Link to='/signup'> Create an Account</Link></p>
           <div className="otherLogin">
             <img onClick={handleGoogleLogin} src={googleIcon} alt="" />
--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,28 +1,59 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "./Signup.css";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../providers/AuthProvider";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Signup = () => {
 
   const {createUser}= useContext(AuthContext)
 
+  const [error, setError]= useState('')
+  
   const handleSingUp = event =>{
+    setError('')
     event.preventDefault()
     const emailField = event.target.email.value;
     const passwordField =event.target.password.value;
     const confirmField =event.target.confirm.value;
 
-    createUser(emailField , passwordField, confirmField)
+    if(passwordField !==confirmField){
+      setError("Password didn't Match")
+      return
+    }
+    else if(passwordField < 6){
+      setError('Password Must be 6 Characters or longer')
+      return
+    }
+
+    createUser(emailField , passwordField)
     .then((result)=>{
-    const newUser = result.user
-      console.log(newUser)
+    const newUser = result.user;
       event.target.reset()
+      toast.success('SignUp Successfull', {
+        position: "top-center",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        });
     })
     .catch(error =>{
-      console.log(error)
+      setError(error.message)
     })
+
   
+  }
+
+  const [show, setShow]= useState(true)
+  const handleshowPassword=()=>{
+    setShow(!show)
   }
   return (
     <div className="signuppage">
@@ -36,14 +67,17 @@ const Signup = () => {
             </div>
             <div className="form-control">
               <label htmlFor="password">Password</label>
-              <input type="password" name="password" required/>
+              <input type={show ? 'password' : 'text'} name="password" required/>
             </div>
             <div className="form-control">
               <label htmlFor="confirm">Confirm Password</label>
-              <input type="password" name="confirm" required/>
+              <input type={show ? 'password' : 'text'} name="confirm" required/>
+              {show? <FontAwesomeIcon onClick={handleshowPassword} icon={faEyeSlash} id='eyeoff'/> : <FontAwesomeIcon onClick={handleshowPassword} icon={faEye} id='eyeoff'/> }
             </div>
+            <p id="errortext">{error}</p>
             <button type="submit" className="submitbtn" value='sign up'>Sign Up</button>
           </form>
+          <ToastContainer />
           <p>Already have an account ? <Link to='/login'> Login</Link></p>
         </div>
       </div>
